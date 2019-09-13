f51c6f7fe1d9ad4d6aa8007711ce61c855c97567
lights <lightsever@hotmail.com>
2019-09-13 18:11:55 +0800

    完成部分 page02 编写

---
+++ samples/neovm01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace neovm01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neo.VM.ScriptBuilder builder = new Neo.VM.ScriptBuilder();
+
+            builder.Emit(Neo.VM.OpCode.NOP);
+            builder.EmitPush(1);
+            builder.EmitPush(2);
+            builder.Emit(Neo.VM.OpCode.ADD);
+            builder.Emit(Neo.VM.OpCode.RET);
+
+            var machinecode = builder.ToArray();
+            var hexstr = "0x";
+            foreach (var m in machinecode)
+            {
+                hexstr += m.ToString("X02");
+            }
+            Console.WriteLine("machinecode=" + hexstr);
+
+            //run machinecode with neovm
+            var engine = new Neo.VM.ExecutionEngine();
+            engine.LoadScript(machinecode);
+            engine.Execute();
+
+            //show result
+            var calcstack = engine.ResultStack;
+
+            var v = calcstack.Peek();
+            Console.WriteLine("retvalue=" + v.GetBigInteger());
+
+            Console.ReadLine();
+
+        }
+    }
+}
